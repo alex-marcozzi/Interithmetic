f9ccd7799a15c3751030bfbdedbc34021105c18e
Alexander Marcozzi <alex.marcozzi1@gmail.com>
2021-08-11 15:41:49 -0500

    Add timer

--- game/engine.py
+++ game/engine.py
@@ -23,15 +23,19 @@ class Engine:
         self.counter       = 0
         self.sample_counts = [0 for i in range(6)]
         self.question_over = False
+        self.time_left     = self.QUESTION_TIME
         
-        self.top_label = pyglet.text.Label(text = "Words go here", color = self.ORANGE, font_name = 'Calibri', font_size = 48,
+        self.top_label = pyglet.text.Label(text = "Words", color = self.ORANGE, font_name = 'Calibri', font_size = 48,
                            x = width // 2, y = height * 0.85, anchor_x = 'center')
         self.q_label = pyglet.text.Label(text = self.qm.questionToString(False), color = self.ORANGE, font_name = 'Calibri', font_size = 48,
                            x = width // 2, y = height * 0.10, anchor_x = 'center')
         self.response = 0
         self.response_label = pyglet.text.Label(text = str(self.response), color = self.ORANGE, font_name = 'Calibri', font_size = 64,
                            x = width * 0.10, y = height * 0.50, anchor_x = 'center')
+        self.time_label = pyglet.text.Label(text = "Time: " + str(self.time_left), color = self.ORANGE, font_name = 'Calibri', font_size = 36,
+                           x = width * 0.85, y = height * 0.85, anchor_x = 'center')
         pyglet.clock.schedule_once(self.endQuestion, self.QUESTION_TIME)
+        pyglet.clock.schedule_interval(self.tickTimer, 1)
     
     def update(self, dt):
         self.counter += 1
@@ -51,17 +55,21 @@ class Engine:
         self.top_label.draw()
         self.q_label.draw()
         self.response_label.draw()
+        self.time_label.draw()
 
     def setUIColor(self, color):
         self.top_label.color      = color
         self.q_label.color        = color
         self.response_label.color = color
+        self.time_label.color     = color
         self.rm.setBorderColor(color[:3])
 
     def endQuestion(self, dt):
         # pause music here, play sound effect
         self.setUIColor(self.WHITE)
         self.question_over = True
+        self.time_left = 0
+        self.time_label.text = "Time: " + str(self.time_left)
         pyglet.clock.schedule_once(self.checkAnswer, 2)
 
     def checkAnswer(self, dt):
@@ -79,7 +87,13 @@ class Engine:
         self.q_label.text = self.qm.questionToString(False)
         self.setUIColor(self.ORANGE)
         self.question_over = False
+        self.time_left = self.QUESTION_TIME
         pyglet.clock.schedule_once(self.endQuestion, self.QUESTION_TIME)
+
+    def tickTimer(self, dt):
+        if self.time_left != 0:
+            self.time_left -= 1
+            self.time_label.text = "Time: " + str(self.time_left)
 
     def cleanUp(self):
         self.rm.cleanUp()
