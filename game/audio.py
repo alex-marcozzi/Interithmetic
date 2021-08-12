876a1a8db406164ef8db6c3fbe26933cdb1c4604
Alexander Marcozzi <alex.marcozzi1@gmail.com>
2021-08-12 17:15:30 -0500

    Add more music

--- game/audio.py
+++ game/audio.py
@@ -10,10 +10,29 @@ awwww = pyglet.media.load('./assets/sfx/awwww.mp3', streaming=False)
 
 # music
 music = [
-    pyglet.media.load('./assets/music/jeopardy.mp3', streaming=False),
-    pyglet.media.load('./assets/music/rose.mp3', streaming=False)
+    pyglet.media.load('./assets/music/jeopardy.mp3', streaming=False),  # Jeopardy song
+    pyglet.media.load('./assets/music/rose.mp3', streaming=False),      # Rondo Brothers, Yellow Flower of Berkely
+    pyglet.media.load('./assets/music/thinking.mp3', streaming=False),  # Kevin MacLeod, Thinking Music
+    pyglet.media.load('./assets/music/nitro.mp3', streaming=False),     # Nitro Fun, Checkpoint
+    pyglet.media.load('./assets/music/last7.mp3', streaming=False),     # oneohkay, Last 7 Letters
+    pyglet.media.load('./assets/music/moon.mp3', streaming=False),      # Gecko&Tokage Parade, Moon
+    pyglet.media.load('./assets/music/beat.mp3', streaming=False),      # Nujabes, Beat laments the world
+    pyglet.media.load('./assets/music/prayer.mp3', streaming=False),    # Nujabes, Prayer
+    pyglet.media.load('./assets/music/rush.mp3', streaming=False),      # The Seatbelts, Rush
+    pyglet.media.load('./assets/music/dog.mp3', streaming=False),       # The Seatbelts, Bad Dog No Biscuits
 ]
 
 # convenience functions
 def randomSong():
-    return music[random.randint(0,1)]
+    return music[random.randint(0,len(music)-1)]
+
+
+
+# NOTES
+# So the actual game part is pretty much done, you should add more music though, maybe like 5 - 10 tracks total.
+# Of course, you still need to train the model with the full range of answers as well. I think you can probably
+# start working on the menus, so maybe add a MenuEngine class or something, which will work a lot like in SSB2,
+# with an enum class for knowing what the game state is (MAIN, CREDITS, DIFFICULTY_SELECT, PAUSED, PLAYING).
+# Also, add another parameter to the Engine constructor, which is the number of seconds per questions. This can
+# be used to implement difficutly settings, like 30 seconds for easy, 10 seconds for normal, and 5 for hard, or
+# something like that. 
