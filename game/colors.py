8d11632328fb569b6dcfd495f86c98b3330d2e08
Alexander Marcozzi <alex.marcozzi1@gmail.com>
2021-11-19 17:01:41 -0600

    Add documentation for colors.py

--- game/colors.py
+++ game/colors.py
@@ -1,5 +1,10 @@
-ORANGE        = (232,74,39,255)
-BLUE          = (19,41,75)
-GREEN         = (0,255,0,255)
-RED           = (255,0,0,255)
-WHITE         = (255,255,255,255)
+# Title: colors.py
+# Description: Contains colors used in Interithmetic, in RGBA format (1 - 255).
+# Author: Alexander Marcozzi
+# Date: 11/19/2021
+
+ORANGE = (232,74,39,255)
+BLUE   = (19,41,75)
+GREEN  = (0,255,0,255)
+RED    = (255,0,0,255)
+WHITE  = (255,255,255,255)
