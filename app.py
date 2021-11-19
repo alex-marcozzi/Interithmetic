57bf990e3c87fba6c1a6c9defaa3134037dbd8aa
Alexander Marcozzi <alex.marcozzi1@gmail.com>
2021-11-19 17:27:06 -0600

    Add app.py as the main driver file (really)

---
+++ app.py
@@ -0,0 +1,26 @@
+from os import stat
+import pyglet
+from pyglet.window import mouse
+from game.state_engine import StateEngine
+
+#game_window = pyglet.window.Window(1000, 750)
+game_window = pyglet.window.Window(fullscreen=True)
+
+state_engine = StateEngine(game_window.width, game_window.height)
+pyglet.clock.schedule_interval(state_engine.update, 0.05)
+
+@game_window.event
+def on_mouse_press(x, y, button, modifiers):
+    state_engine.handleClick(x, y, button, modifiers)
+
+@game_window.event
+def on_draw():
+    game_window.clear()
+
+    state_engine.draw()
+
+if __name__ == '__main__':
+    pyglet.app.run()
+
+game_window.close()
+state_engine.cleanUp()
